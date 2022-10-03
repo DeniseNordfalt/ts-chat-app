63c672918cc8cde00ee53399ec9c1d03edbbe250
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-03 13:50:58 +0200

    added mongoose and mongodb

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,17 +1,21 @@
-import express, { Application, json, Request, Response } from "express";
 import cors from "cors";
+import express, { Application, json } from "express";
+import dotenv from "dotenv";
+import messagesController from "./controllers/messages-controller";
+import { setupMongoDb } from "./models/chat-repository";
+
+dotenv.config();
+
 const app: Application = express();
-app.use(cors());
+app.use(cors()); // TODO Configure CORS properly to make the app secure.
 app.use(json());
-const port: number = parseInt(process.env.SERVER_PORT || `3001`);
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
-});
+const port: number = parseInt(process.env.SERVER_PORT || "3001");
+const mongoUrl: string =
+  process.env.MONGODB_URL || "mongodb://localhost:27017/chat";
 
-app.get("/api", (req: Request, res: Response) => {
-  res.send("Hello, API!");
-});
+app.use("/messages", messagesController);
 
-app.listen(port, function () {
+app.listen(port, async function () {
+  await setupMongoDb(mongoUrl);
   console.log(`App is listening on port ${port} !`);
 });
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,24 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+import MessageItem from "@ts-chat-app/shared";
+
+axios.defaults.baseURL =
+  process.env.REACT_APP_TODO_API || "http://localhost:3001";
+
+const fetchMessages = async (): Promise<MessageItem[]> => {
+  const response = await axios.get<MessageItem[]>("/messages");
+  return response.data;
+};
+
+const MessageList = ({
+  messages,
+  error,
+}: {
+  messages: MessageItem[];
+  error?: string;
+}) => {
+  if (error) {
+    return <div>{error}</div>;
+  } else if (messages) {
+    return (
+      <div>
+        {messages.map((item) => {
+          return <p key={item.id}>{item.text}</p>;
+        })}
+      </div>
+    );
+  } else {
+    return <div>'Waiting for messages'</div>;
+  }
+};
+
+const MessageInput = ({
+  messageText,
+  setMessageText,
+  onCreate,
+}: {
+  messageText: string;
+  setMessageText: (text: string) => void;
+  onCreate: (text: string) => void;
+}) => {
+  return (
+    <>
+      <input
+        type="text"
+        value={messageText}
+        onChange={(e) => setMessageText(e.target.value)}
+      />
+      <button onClick={(e) => onCreate(messageText)}>Create message</button>
+    </>
+  );
+};
 
 function App() {
+  const [messageText, setMessageText] = useState<string>("");
+  const [messages, setMessages] = useState<MessageItem[]>([]);
+  const [error, setError] = useState<string | undefined>();
+
+  const createMessage = async (messageText: string): Promise<void> => {
+    const messageItem: MessageItem = {
+      text: messageText,
+      timeStamp: new Date(),
+    };
+
+    try {
+      await axios.post("/messages", messageItem);
+      const response = await axios.get<MessageItem[]>("/messages");
+      setMessages(response.data);
+    } catch (err) {
+      setMessages([]);
+      setError("Something went wrong when fetching my messages...");
+    } finally {
+      setMessageText("");
+    }
+  };
+
+  useEffect(() => {
+    fetchMessages()
+      .then(setMessages)
+      .catch((error) => {
+        setMessages([]);
+        setError("Something went wrong when fetching my messages...");
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">My Message Lists</header>
+      <section className="App-content">
+        <MessageList messages={messages} error={error} />
+      </section>
+      <footer className="App-footer">
+        <MessageInput
+          onCreate={createMessage}
+          setMessageText={setMessageText}
+          messageText={messageText}
+        />
+      </footer>
     </div>
   );
 }
