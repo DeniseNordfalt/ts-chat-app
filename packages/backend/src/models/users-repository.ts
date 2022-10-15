dfe4da79a3fa956f1ae7932310de1a916653ace0
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-15 23:24:11 +0200

    added presave, changes name to User, create user

--- packages/backend/src/models/users-repository.ts
+++ packages/backend/src/models/users-repository.ts
@@ -1,24 +1,48 @@
-import UserItem from "../user-item";
+import { UserItem } from "@ts-chat-app/shared";
 import { model, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
-const UserSchema = new Schema({
-  name: String,
-  password: String,
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-const UserModel = model<UserItem>("UserItem", UserSchema);
+// hashes the password before saving the user
+userSchema.pre(/save/, async function (next): Promise<void> {
+  if (this.modifiedPaths().includes("password")) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+  next();
+});
 
-export const loadAllUserItems = async (): Promise<UserItem[]> => {
-  return UserModel.find({}).exec();
+//saves the user to the database
+export const saveUserItem = async (user: UserItem): Promise<UserItem> => {
+  const newUser = await User.create(user);
+  return newUser;
+  //TODO: add error handling
 };
 
+const User = model<UserItem>("UserItem", userSchema);
+
+// loads all users
+export const loadAllUserItems = async (): Promise<UserItem[]> => {
+  return User.find({}).exec();
+  //TODO: add error handling, remove password from response
+};
+
+//loads a user by id
 export const loadUserItem = async (
   userId: string
 ): Promise<UserItem | null> => {
-  return UserModel.findById(userId).exec();
+  return User.findById(userId).exec();
+  //TODO: add error handling, remove password from response
 };
 
-export const saveUserItem = async (userItem: UserItem): Promise<void> => {
-  const newModel = new UserModel(userItem);
-  newModel.save();
+//loads a user by username
+export const loadUserItemByUsername = async (
+  username: string
+): Promise<UserItem | null> => {
+  return await User.findOne({ username: username }).exec();
 };
