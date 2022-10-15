e0d937a97bb1d842903241a0bf84a1c0012b4f10
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-15 23:27:21 +0200

    imported shared, changed to get, load by username

--- packages/backend/src/services/users-service.ts
+++ packages/backend/src/services/users-service.ts
@@ -1,34 +1,41 @@
-import UserItem from "../user-item";
+// import UserItem from "../user-item";
+import { UserItem } from "@ts-chat-app/shared";
 import {
   loadAllUserItems,
   loadUserItem,
   saveUserItem,
+  loadUserItemByUsername,
 } from "../models/users-repository";
 
-export const saveUser = async (userItem: UserItem): Promise<UserItem[]> => {
-  if (!userItem.name || userItem.name == "") {
+export const saveUser = async (UserItem: UserItem): Promise<UserItem> => {
+  if (!UserItem.username || UserItem.username == "") {
     throw new Error("Invalid name on user item!");
   }
 
-  if (!userItem.password || userItem.password == "") {
+  if (!UserItem.password || UserItem.password == "") {
     throw new Error("Invalid password on user item!");
   }
 
-  await saveUserItem(userItem);
+  await saveUserItem(UserItem);
+  return UserItem;
+};
 
+export const getUsers = async (): Promise<UserItem[]> => {
   return await loadAllUserItems();
 };
 
-export const loadUsers = async (): Promise<UserItem[]> => {
-  return await loadAllUserItems();
-};
-
-export const loadItemById = async (userId: string): Promise<UserItem> => {
-  const item = await loadUserItem(userId);
+export const getUserById = async (id: string): Promise<UserItem> => {
+  const item = await loadUserItem(id);
 
   if (!item) {
-    throw new Error(`Can't find item with id ${userId}`);
+    throw new Error(`Can't find item with id ${id}`);
   }
 
   return item;
+};
+
+export const getUserByUsername = async (
+  username: string
+): Promise<UserItem | null> => {
+  return await loadUserItemByUsername(username);
 };
