b7d419bc2ad91730bd788470e0e80c3e62748324
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-15 23:25:47 +0200

    changed to shared userite, changed name to get

--- packages/backend/src/controllers/users-controllers.ts
+++ packages/backend/src/controllers/users-controllers.ts
@@ -1,18 +1,20 @@
-import UserItem from "../user-item";
+// import UserItem from "../user-item";
+import { UserItem } from "@ts-chat-app/shared";
 import { Router, Request, Response } from "express";
-import { loadItemById, loadUsers, saveUser } from "../services/users-service";
+import { getUsers, saveUser, getUserById } from "../services/users-service";
 
 const usersController = Router();
 
 usersController.get("/", async (req: Request, res: Response<UserItem[]>) => {
-  res.send(await loadUsers());
+  res.send(await getUsers());
 });
 
 usersController.get(
   "/:userId",
   async (req: Request, res: Response<UserItem>) => {
     try {
-      res.send(await loadItemById(req.params.userId));
+      res.send(await getUserById(req.params.userId));
+      console.log("User loaded", req.params.userId);
     } catch (e) {
       res.sendStatus(404);
     }
@@ -21,7 +23,7 @@ usersController.get(
 
 usersController.post(
   "/",
-  async (req: Request<UserItem>, res: Response<UserItem[]>) => {
+  async (req: Request<UserItem>, res: Response<UserItem>) => {
     try {
       res.send(await saveUser(req.body));
     } catch (e) {
