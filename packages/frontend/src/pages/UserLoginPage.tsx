c2000789ad7079c7902887ba5a20a0335ffcf83e
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-23 13:45:08 +0200

    first commit user login

---
+++ packages/frontend/src/pages/UserLoginPage.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import FormInput from "../components/molecules/FormInput";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:4000";
+
+type Props = {};
+
+export default function UserLoginPage({}: Props) {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const loginUser = async (username: string, password: string) => {
+    const user = {
+      username: username,
+      password: password,
+    };
+    try {
+      const response = await axios.post("/login", user);
+      console.log(response.data);
+      if (response.data.token) {
+        localStorage.setItem("jwt_token", response.data.token);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setUsername("");
+      setPassword("");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          loginUser(username, password);
+        }}
+      >
+        <div>
+          <FormInput
+            id="username"
+            label="Username"
+            value={username}
+            setValue={setUsername}
+          />
+
+          <FormInput
+            id="password"
+            label="Password"
+            value={password}
+            setValue={setPassword}
+            type="password"
+          />
+
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      <div>{username}</div>
+    </div>
+  );
+}
