63ff5b63d1a3787746274f99bda5ab7b07c972d6
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-23 13:45:34 +0200

    basic 404

---
+++ packages/frontend/src/pages/NotFound.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Flex, Heading, Stack } from "@chakra-ui/react";
+
+type Props = {};
+
+export default function NotFound({}: Props) {
+  return (
+    <Flex minH={"100vh"} align={"center"} justify={"center"} bg={"gray.50"}>
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} color={"red.600"}>
+            404
+          </Heading>
+          <Heading fontSize={"2xl"}>Page not found</Heading>
+        </Stack>
+      </Stack>
+    </Flex>
+  );
+}
