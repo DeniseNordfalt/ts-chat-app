2617e2764c9bea984ff8585ff2401807409ff1d6
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-30 16:09:50 +0100

    changes to chatpage

--- packages/frontend/src/pages/ChatPage.tsx
+++ packages/frontend/src/pages/ChatPage.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { MessageItem } from "@ts-chat-app/shared";
 import { Input, Button, Flex } from "@chakra-ui/react";
+import ChatInput from "../components/molecules/ChatInput";
 
 import {
   useEventSource,
@@ -48,8 +49,14 @@ export default function ChatPage({}: Props) {
     } else if (messages) {
       return (
         <div>
-          {messages.map((item) => {
-            return <p key={item._id}>{item.text}</p>;
+          {messages.map((item, index) => {
+            return (
+              // <div key={item._id}>
+              <div key={index}>
+                <p>{item.author}</p>
+                <p>{item.text}</p>
+              </div>
+            );
           })}
         </div>
       );
@@ -58,35 +65,35 @@ export default function ChatPage({}: Props) {
     }
   };
 
-  const MessageInput = ({
-    messageText,
-    setMessageText,
-    onCreate,
-  }: {
-    messageText: string;
-    setMessageText: (text: string) => void;
-    onCreate: (text: string) => void;
-  }) => {
-    return (
-      <>
-        <Flex
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          w="100%"
-          p={4}
-          //   bg="gray.100"
-        >
-          <Input
-            value={messageText}
-            onChange={(e) => setMessageText(e.target.value)}
-          />
+  // const MessageInput = ({
+  //   messageText,
+  //   setMessageText,
+  //   onCreate,
+  // }: {
+  //   messageText: string;
+  //   setMessageText: (text: string) => void;
+  //   onCreate: (text: string) => void;
+  // }) => {
+  //   return (
+  //     <>
+  //       <Flex
+  //         direction="row"
+  //         justifyContent="space-between"
+  //         alignItems="center"
+  //         w="100%"
+  //         p={4}
+  //         //   bg="gray.100"
+  //       >
+  //         <Input
+  //           value={messageText}
+  //           onChange={(e) => setMessageText(e.target.value)}
+  //         />
 
-          <Button onClick={(e) => onCreate(messageText)}>Send message</Button>
-        </Flex>
-      </>
-    );
-  };
+  //         <Button onClick={(e) => onCreate(messageText)}>Send message</Button>
+  //       </Flex>
+  //     </>
+  //   );
+  // };
 
   const createMessage = async (messageText: string): Promise<void> => {
     const messageItem: MessageItem = {
@@ -131,8 +138,8 @@ export default function ChatPage({}: Props) {
         <MessageList messages={messages} error={error} />
       </section>
       <footer className="App-footer">
-        <MessageInput
-          onCreate={createMessage}
+        <ChatInput
+          onCreate={() => createMessage(messageText)}
           setMessageText={setMessageText}
           messageText={messageText}
         />
