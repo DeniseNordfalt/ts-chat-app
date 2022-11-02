bba34c0798c666bc274397c56b31b3d17410b3ea
DeniseNordfalt <kaoskatt@gmail.com>
2022-11-02 11:24:12 +0100

    changed routes

---
+++ packages/backend/src/controllers/users-service.ts
@@ -0,0 +1,45 @@
+// import UserItem from "../user-item";
+import { UserItem } from "@ts-chat-app/shared";
+import {
+  loadAllUserItems,
+  loadUserById,
+  saveUserItem,
+  loadUserItemByUsername,
+} from "../models/users-repository";
+
+//saves a new user
+export const saveUser = async (UserItem: UserItem): Promise<UserItem> => {
+  if (!UserItem.username || UserItem.username == "") {
+    throw new Error("Invalid name on user item!");
+  }
+
+  if (!UserItem.password || UserItem.password == "") {
+    throw new Error("Invalid password on user item!");
+  }
+
+  await saveUserItem(UserItem);
+  return UserItem;
+};
+
+//loads all users
+export const getUsers = async (): Promise<UserItem[]> => {
+  return await loadAllUserItems();
+};
+
+//loads a user by id
+export const getUserById = async (id: string): Promise<UserItem> => {
+  const item = await loadUserById(id);
+
+  if (!item) {
+    throw new Error(`Can't find item with id ${id}`);
+  }
+
+  return item;
+};
+
+//loads a user by username
+export const getUserByUsername = async (
+  username: string
+): Promise<UserItem | null> => {
+  return await loadUserItemByUsername(username);
+};
