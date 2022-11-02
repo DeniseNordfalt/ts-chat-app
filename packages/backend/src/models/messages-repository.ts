2065815730ea27efc077f70672ca192865be62ef
DeniseNordfalt <kaoskatt@gmail.com>
2022-11-02 11:24:23 +0100

    commenting

--- packages/backend/src/models/messages-repository.ts
+++ packages/backend/src/models/messages-repository.ts
@@ -8,16 +8,19 @@ const MessageSchema = new Schema({
 
 const MessageModel = model<MessageItem>("MessageItem", MessageSchema);
 
+//loads all messages
 export const loadAllMessageItems = async (): Promise<MessageItem[]> => {
   return MessageModel.find({}).exec();
 };
 
+//loads a message by id
 export const loadMessageItem = async (
   messageId: string
 ): Promise<MessageItem | null> => {
   return MessageModel.findById(messageId).exec();
 };
 
+//saves a new message
 export const saveMessageItem = async (
   messageItem: MessageItem
 ): Promise<void> => {
