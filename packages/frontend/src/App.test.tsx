df222c883b8699388d6165e67fafe4f7c71254c4
Denise Nordfalt <denisenordfalt@gmail.com>
2022-11-11 02:55:30 +0100

    test

--- packages/frontend/src/App.test.tsx
+++ packages/frontend/src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import UserCreatePage from "./pages/UserCreatePage";
 
@@ -10,6 +10,39 @@ test("renders user create page", () => {
       <UserCreatePage />
     </Router>
   );
-  const headerElement = screen.getByText(/Create User/i);
-  expect(headerElement).toBeInTheDocument();
+
+  const usernameElement = screen.getByLabelText(/Username/i);
+  expect(usernameElement).toBeInTheDocument();
+
+  const passwordElement = screen.getByLabelText(/Password/i);
+  expect(passwordElement).toBeInTheDocument();
+
+  const emailElement = screen.getByLabelText(/Email/i);
+  expect(emailElement).toBeInTheDocument();
+
+  const submitElement = screen.getByText(/Register/i);
+  expect(submitElement).toBeInTheDocument();
+});
+
+test("changes values on form input", () => {
+  render(
+    <Router>
+      <UserCreatePage />
+    </Router>
+  );
+
+  const usernameElement = screen.getByLabelText(/Username/i);
+  userEvent.type(usernameElement, "test");
+
+  const passwordElement = screen.getByLabelText(/Password/i);
+  userEvent.type(passwordElement, "test");
+
+  const emailElement = screen.getByLabelText(/Email/i);
+  userEvent.type(emailElement, "test@test.se");
+
+  expect(usernameElement).toHaveValue("test");
+  expect(passwordElement).toHaveValue("test");
+  expect(emailElement).toHaveValue("test@test.se");
+
+  expect(usernameElement).not.toHaveValue("testuser2");
 });
