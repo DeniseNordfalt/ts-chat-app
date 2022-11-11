eff790b8fafcd879ea2650ff14263a94db74b896
Denise Nordfalt <denisenordfalt@gmail.com>
2022-11-11 03:07:04 +0100

    changed

--- packages/frontend/src/App.test.tsx
+++ packages/frontend/src/App.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import UserCreatePage from "./pages/UserCreatePage";
 
@@ -22,27 +21,4 @@ test("renders user create page", () => {
 
   const submitElement = screen.getByText(/Register/i);
   expect(submitElement).toBeInTheDocument();
-});
-
-test("changes values on form input", () => {
-  render(
-    <Router>
-      <UserCreatePage />
-    </Router>
-  );
-
-  const usernameElement = screen.getByLabelText(/Username/i);
-  userEvent.type(usernameElement, "test");
-
-  const passwordElement = screen.getByLabelText(/Password/i);
-  userEvent.type(passwordElement, "test");
-
-  const emailElement = screen.getByLabelText(/Email/i);
-  userEvent.type(emailElement, "test@test.se");
-
-  expect(usernameElement).toHaveValue("test");
-  expect(passwordElement).toHaveValue("test");
-  expect(emailElement).toHaveValue("test@test.se");
-
-  expect(usernameElement).not.toHaveValue("testuser2");
 });
